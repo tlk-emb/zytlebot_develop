a8703c18109da7dc323413a5f838509ee95f5f32
sousou1 <tamusou@gmail.com>
2018-12-06 10:04:45 +0900

    pcamのnodelet化

--- autorace/src/autorace.cpp
+++ autorace/src/autorace.cpp
@@ -1,4 +1,6 @@
 #include <pluginlib/class_list_macros.h>
 #include "autorace/nodelet_autorace.h"
+#include "autorace/nodelet_pcam.h"
 
 PLUGINLIB_EXPORT_CLASS(autorace::NodeletAutorace, nodelet::Nodelet)
+PLUGINLIB_EXPORT_CLASS(autorace::NodeletPcam, nodelet::Nodelet)
