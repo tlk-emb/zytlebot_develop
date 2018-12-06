8012e38469879f84c883e93cdc91cda9a8cb06b7
sousou1 <tamusou6953@gmail.com>
2018-12-06 20:54:57 +0900

    nodelet_signal作成

--- autorace/src/autorace.cpp
+++ autorace/src/autorace.cpp
@@ -2,7 +2,9 @@
 #include "autorace/nodelet_autorace.h"
 #include "autorace/nodelet_pcam.h"
 #include "autorace/nodelet_usbcam.h"
+#include "autorace/nodelet_signal.h"
 
 PLUGINLIB_EXPORT_CLASS(autorace::NodeletAutorace, nodelet::Nodelet)
 PLUGINLIB_EXPORT_CLASS(autorace::NodeletPcam, nodelet::Nodelet)
 PLUGINLIB_EXPORT_CLASS(autorace::NodeletUsbcam, nodelet::Nodelet)
+PLUGINLIB_EXPORT_CLASS(autorace::NodeletSignal, nodelet::Nodelet)
